bc985b91c80b45e033a30de8dff28e1f938f0806
Никита Молчанов <nylesszpg@gmail.com>
2023-02-16 14:45:23 +0300

    readme and pip freeze

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,10 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-=======
     'users.apps.UsersConfig',
->>>>>>> 7469c9edc63157c1157756acfab05280127429ce
     'rest_framework',
     'rest_framework_simplejwt',
 ]
