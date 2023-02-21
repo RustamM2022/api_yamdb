8bf80609aab677a2ecc270908c52afaa4390a195
Александров Илья <ybalnagoi@gmail.com>
2023-02-21 13:51:22 +0300

    77

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import CommentSerializer, ReviewSerializer
 from users.permissions import IsSuperUserIsAdminIsModeratorIsAuthor
 
-from reviews.models import Review, Title
+from reviews.models import Review, Title, Comments
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (IsSuperUserIsAdminIsModeratorIsAuthor,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsSuperUserIsAdminIsModeratorIsAuthor
+    )
 
     def get_title(self):
-        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title
 
     def get_queryset(self):
-        return self.get_title().reviews.all()
+        title_id = self.kwargs.get("title_id")
+        reviews = Review.objects.filter(title=title_id)
+        return reviews
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, title=self.get_title())
@@ -23,16 +29,19 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsSuperUserIsAdminIsModeratorIsAuthor,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsSuperUserIsAdminIsModeratorIsAuthor
+    )
 
-    # вернет отзыв
     def get_review(self):
-        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review
 
-    # Получаем все комменты произведения
     def get_queryset(self):
-        return self.get_review().comments.all()
+        review_id = self.kwargs.get("review_id")
+        comments = Comments.objects.filter(review=review_id)
+        return comments
 
-    # создаст коммент, если соблюдены условия
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, review=self.get_review())
